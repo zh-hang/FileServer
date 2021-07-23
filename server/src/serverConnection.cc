5974f7dcbcc33f40d90b445daa06d19b740eec9b
zhang-hang <zhang-hang@example.com>
2021-07-23 07:18:21 +0000

    添加过程标识

--- server/src/serverConnection.cc
+++ server/src/serverConnection.cc
@@ -6,11 +6,14 @@ ServerConnection::ServerConnection(int type):Connection{type}
 		writeLog("bind failed.\n");
 		exit(0);
 	}
+    std::cout<<"bind successfully.\n";
 	if(listen(this->fd,QUEQUE_SIZE)<0)
 		writeLog("listen failed.\n");
+    std::cout<<"listen successfully.\n";
 }
 
 int ServerConnection::acceptTCP(){
+    std::cout<<"start accept.\n";
 	int connfd=accept(this->fd,0,0);
 	if(connfd<0)
 		writeLog("accept failed.\n");
