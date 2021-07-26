68c6c2f5811c426eeae1d5ee673046ce84741f08
zhang-hang <zhang-hang@example.com>
2021-07-26 08:26:58 +0000

    完成了对话传输和文件传输的测试，现在开始系统测试

--- client/main.cc
+++ client/main.cc
@@ -7,7 +7,6 @@
 #include "src/clientConnection.h"
 #include <vector>
 
-#define PORT 41414
 
 std::string address="139.9.202.175";
 
@@ -15,27 +14,23 @@ int main(int argc, char* argv[])
 {
     if(argc==2)
         address=argv[1];
-    // ClientConnection conn(TCP_TYPE,address);
-    // ClientConnection::sendMsg(conn.fd,"hello");
-    // std::string msg;
-    // std::vector<std::string>fileList;
-    // while(1) {
-    //     msg=ClientConnection::recvMsg(conn.fd);
-    //     if(msg.compare(0,5,"file:")==0){
-    //         std::cout<<"receive: "<<msg.substr(5)<<std::endl;
-    //         fileList.push_back(msg.substr(5));
-    //     }
-    //     else
-    //         break;
-    // }
-    // ClientConnection::sendMsg(conn.fd,fileList[0]);
-    std::cout<<address<<std::endl;
-    ClientConnection fileTrans(UDP_TYPE,address,23456);
-    
-    ClientConnection::sendMsg(fileTrans.fd,"log.txt",(sockaddr*)&(fileTrans.addr));
-    std::cout<<"start recv file\n";
-    while(1){
-        fileTrans.recvFileUDP("test.txt");
+    ClientConnection conn(TCP_TYPE,address);
+    std::string msg;
+    std::vector<std::string>fileList;
+    while(1) {
+        msg=ClientConnection::recvMsg(conn.fd);
+        if(msg.compare(0,5,"file:")==0){
+            std::cout<<"receive: "<<msg.substr(5)<<std::endl;
+            fileList.push_back(msg.substr(5));
+        }
+        else
+            break;
     }
+    ClientConnection::sendMsg(conn.fd,fileList[0]);
+    int port =std::stoi(ClientConnection::recvMsg(conn.fd));
+    ClientConnection fileTrans(UDP_TYPE,address,port);    
+    std::cout<<"start recv file\n";
+    fileTrans.recvFileUDP(fileList[0]);
+    
     return 0;
 }
--- server/main.cc
+++ server/main.cc
@@ -15,26 +15,25 @@
 
 int main(){
 	FileManager fm;
-	// ServerConnection main_connection(TCP_TYPE);
+	ServerConnection main_connection(TCP_TYPE);
     int UDP_Port=23456;
     ServerConnection send_file_connection(UDP_TYPE,UDP_Port);
-    sockaddr*sa;
-    std::string filename=ServerConnection::recvMsg(send_file_connection.fd,sa);
-    send_file_connection.sendFileUDP(filename, sa);
-	// while(true){
-	// 	int fd=main_connection.acceptTCP();
-    //     std::cout<<ServerConnection::recvMsg(fd);
-    //     std::vector<std::string> filelist=fm.getFilesList();
-	// 	for(auto file:filelist){
-    //         ServerConnection::sendMsg(fd,"file:"+file);
-    //     }
-    //     ServerConnection::sendMsg(fd,"finish");
-    //     std::string send_filename(ServerConnection::recvMsg(fd));
-    //     sockaddr*ra;
-    //     socklen_t len;
-    //     getpeername(fd, ra, &len);
-    //     send_file_connection.sendFileUDP(send_filename, ra);
-	// 	close(fd);
-	// }
+	while(true){
+		int fd=main_connection.acceptTCP();
+        std::cout<<ServerConnection::recvMsg(fd);
+        std::vector<std::string> filelist=fm.getFilesList();
+		for(auto file:filelist){
+            ServerConnection::sendMsg(fd,"file:"+file);
+        }
+        ServerConnection::sendMsg(fd,"finish");
+        std::string send_filename(ServerConnection::recvMsg(fd));
+        ServerConnection::sendMsg(fd,std::to_string(UDP_Port));
+        sockaddr*ra=new sockaddr();
+        socklen_t len=sizeof(*ra);
+        bzero(ra,len);
+        getpeername(fd, ra, &len);
+        send_file_connection.sendFileUDP(send_filename, ra);
+		close(fd);
+	}
 	return 0;
 }
