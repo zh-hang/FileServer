66498a86fe8d85e3b8c10f5f67c114fb6e5a8f70
zh-hang <zhangh127@163.com.com>
2021-07-28 07:12:36 +0000

    main使用threadpool，待测试

--- server/main.cc
+++ server/main.cc
@@ -11,28 +11,40 @@
 
 #include "../lib/log.h"
 #include "../lib/fileManager.h"
+#include "../lib/threadpool.h"
 
+#define MAX_UDP 16
+
+void dealConnection(int fd,FileManager &fm,ServerConnection &file_connection){
+    std::cout<<ServerConnection::recvMsg(fd);
+    std::vector<std::string> filelist=fm.getFilesList();
+    for(auto file:filelist){
+        ServerConnection::sendMsg(fd,"file:"+file);
+    }
+    ServerConnection::sendMsg(fd,"finish");
+    std::string send_filename(ServerConnection::recvMsg(fd));
+    ServerConnection::sendMsg(fd,std::to_string(file_connection.port));
+    sockaddr*ra=new sockaddr();
+    ServerConnection::recvMsg(file_connection.fd,ra);
+    file_connection.sendFileUDP(send_filename, ra);
+    delete ra;
+    close(fd);
+}
 
 int main(){
+    ThreadPool pool;
 	FileManager fm;
 	ServerConnection main_connection(TCP_TYPE);
-    int UDP_Port=23456;
-    ServerConnection send_file_connection(UDP_TYPE,UDP_Port);
+    std::vector<ServerConnection>file_connections;
+    int base_port=10000;
+    for(int i=0;i<MAX_NUM;i++){
+        file_connections.push_back(ServerConnection(UDP_TYPE,base_port+i));
+    }
+    int i=0;
 	while(true){
 		int fd=main_connection.acceptTCP();
-        std::cout<<ServerConnection::recvMsg(fd);
-        std::vector<std::string> filelist=fm.getFilesList();
-		for(auto file:filelist){
-            ServerConnection::sendMsg(fd,"file:"+file);
-        }
-        ServerConnection::sendMsg(fd,"finish");
-        std::string send_filename(ServerConnection::recvMsg(fd));
-        ServerConnection::sendMsg(fd,std::to_string(UDP_Port));
-        sockaddr*ra=new sockaddr();
-        ServerConnection::recvMsg(send_file_connection.fd,ra);
-        send_file_connection.sendFileUDP(send_filename, ra);
-        delete ra;
-		close(fd);
+        pool.commit(dealConnection, fd,fm,file_connections[i]);
+        i=(i+1)%MAX_NUM;
 	}
 	return 0;
 }
