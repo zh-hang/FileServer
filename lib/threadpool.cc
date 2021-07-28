596fd5caf6af0db0eb99d47c56ee2952a7bc2692
zhang-hang <zhang-hang@example.com>
2021-07-28 06:02:01 +0000

    添加了线程池库

--- lib/threadpool.cc
+++ lib/threadpool.cc
@@ -1,1 +1,47 @@
 #include "threadpool.h"
+
+template<class F,class...Args>
+auto ThreadPool::commit(F&&f,Args&&...args) ->std::future<decltype(f(args...))>{
+    if(!this->_run){
+        std::cout<<"thread pool is stopped.\n";
+        exit(0);
+    }
+    using RetType=decltype(f(args...));
+    auto task=std::make_shared<std::packaged_task<RetType()>>(
+        std::bind(std::forward<F>(f),std::forward<Args>(args)...)
+    );
+    std::future<RetType> future = task->get_future();
+    {
+        std::lock_guard<std::mutex> lock{ this->_lock };
+        this->_tasks.emplace([task](){
+           (*task)();
+        });
+    }
+    this->_task_cv.notify_one();
+    return future;
+}
+
+void ThreadPool::addThread(int num){
+    for (; this->_thread_pool.size() < MAX_NUM && num > 0; --num){
+        this->_thread_pool.emplace_back( [this]{
+            while (_run)
+            {
+                Task task;
+                {
+                    std::unique_lock<std::mutex> lock{ _lock };
+                    _task_cv.wait(lock, [this]{
+                            return !_run || !_tasks.empty();
+                    });
+                    if (!_run && _tasks.empty())
+                        return;
+                    task = move(_tasks.front());
+                    _tasks.pop();
+                }
+                this->_free_thread_num--;
+                task();
+                this->_free_thread_num++;
+            }
+        });
+        this->_free_thread_num++;
+    }    
+}
--- lib/threadpool.h
+++ lib/threadpool.h
@@ -1,16 +1,59 @@
 #include <thread>
 #include <vector>
+#include <functional>
+#include <atomic>
+#include <queue>
+#include <condition_variable>
+#include <mutex>
+#include <iostream>
+#include <memory>
+#include <future>
 
 #ifndef THREAD_POOL_CLASS
 #define THREAD_POOL_CLASS
 
 #define MAX_NUM 32
 
-class ThreadPool{
-    std::vector<std::thread>_thred_pool;
-public:
-    ThreadPool(){}
-    ~ThreadPool(){}
-};
+using Task=std::function<void()>;
 
+class ThreadPool{
+
+    std::vector<std::thread>_thread_pool;
+    std::queue<Task>_tasks;
+    std::mutex _lock;
+    std::condition_variable _task_cv;
+    std::atomic<bool> _run;
+    std::atomic<int> _free_thread_num;
+
+public:
+    ThreadPool(){
+        this->_run=true;
+        this->_free_thread_num=0;
+        this->addThread(MAX_NUM);
+    }
+    ~ThreadPool(){
+        this->_run=false;
+        this->_task_cv.notify_all();
+        for(auto& t:this->_thread_pool){
+            if(t.joinable())
+                t.join();
+        }
+    }
+
+    template<class F,class...Args>
+    auto commit(F&&f,Args&&...args) ->std::future<decltype(f(args...))>;
+
+    int freeThreadCount(){
+        return this->_free_thread_num;
+    }
+    int threadCount(){
+        return this->_thread_pool.size();
+    }
+
+#ifndef GPOWABLE
+private:
+#endif
+    void addThread(int num);
+
+};
 #endif
