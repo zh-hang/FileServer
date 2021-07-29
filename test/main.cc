8b7a13de34b4fb7441f87132ad4ff3ca104759b3
zh-hang <zhangh127@163.com.com>
2021-07-29 07:08:17 +0000

    测试可变参数模板

--- test/main.cc
+++ test/main.cc
@@ -1,16 +1,25 @@
 #include <iostream>
-#include "../lib/threadpool.cc"
+#include <future>
+
+class test{
+    public:
+    template<class F,class ...Args>
+    void print(F&&f,Args&&...args);
+};
+
+
+template<class F,class ...Args>
+void test::print(F&&f,Args&&...args){
+    *f(args...);
+}
 
 int foo(int a,int b,int c){
     return a+b+c;
 }
 
 int main(){
-    ThreadPool pool;
     int a(1),b(2),c(3);
-    pool.commit(foo, a,b,c);
-    while(1){
-        ;
-    }
+    test t;
+    t(&foo,a,b,c);
     return 0;
 }
