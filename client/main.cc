a5759efce1549b97db1076517f8ba166591a5d1d
zh-hang <zhangh127@163.com.com>
2021-08-27 08:42:13 +0000

    按照connection的重构完善client

--- client/main.cc
+++ client/main.cc
@@ -1,96 +1,7 @@
-#include <asm-generic/errno-base.h>
-#include <asm-generic/errno.h>
-#include <cerrno>
-#include <cstddef>
-#include <fstream>
-#include <iostream>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <string>
-#include <unistd.h>
-#include <vector>
 
-#include "src/clientConnection.h"
-
-#include "../lib/typedef.h"
+#include "src/client.h"
 
 #define DEBUG_TIME
-
-void showCommand(){
-    std::cout<<CLIENT_EOF<<":quit"<<std::endl;
-    std::cout<<DOWLOAD_FILE<<":download"<<std::endl;
-    std::cout<<UPLOAD_FILE<<":upload"<<std::endl;
-}
-
-void printFileList(std::vector<std::string>&fileList){
-    for(size_t i =0;i<fileList.size();i++){
-        std::cout<<i<<":"<<fileList[i]<<std::endl;
-    }
-}
-
-void recvFile(int fd,std::string &address,std::vector<std::string>&fileList){
-    printFileList(fileList);
-    std::cout<<"select witch file you want to download.\n";
-    int i;
-    std::cin>>i;
-    if(i<fileList.size()){
-        ClientConnection::sendMsg(fd,fileList[i]);
-        int port =std::stoi(ClientConnection::recvMsg(fd));
-        ClientConnection fileTrans(UDP_TYPE,address,port);
-        ClientConnection::sendMsg(fileTrans.fd,"ready",(sockaddr*)&fileTrans.addr);
-        std::cout<<"start recv file: "<<fileList[i]<<std::endl;
-        fileTrans.recvFileUDP(fileList[i]);
-        fileTrans.close_self();
-    }else{
-        std::cout<<"there is no such file.\n";
-    }
-}
-
-void sendFile(int fd,std::string &address,std::vector<std::string>&fileList){
-    std::string filename;
-    std::cin>>filename;
-    std::string clear_filename;
-    for(size_t c=0;c<filename.length();c++){
-        if(filename[c]=='.'||filename[c]=='/')
-            continue;
-        clear_filename=filename.substr(c);
-        break;
-    }
-    for(auto file:fileList){
-        if (clear_filename==file) {
-            std::cout<<"file exist in server.\n";
-            return ;
-        }
-    }
-    std::ifstream infile(filename);
-    if(!infile){
-        std::cout<<"file not exitst.\n";
-        infile.close();
-        return ;
-    }
-    infile.close();
-    ClientConnection::sendMsg(fd,clear_filename);
-    int port =std::stoi(ClientConnection::recvMsg(fd));
-    ClientConnection fileTrans(UDP_TYPE,address,port);
-    ClientConnection::sendMsg(fileTrans.fd,"ready",(sockaddr*)&fileTrans.addr);
-    fileTrans.sendFileUDP(filename,(sockaddr*)&fileTrans.addr);
-    fileTrans.close_self();
-}
-
-void delFile(int fd,std::vector<std::string>&fileList){
-    printFileList(fileList);
-    std::cout<<"select witch file you want to delete.\n";
-    int i;
-    std::cin>>i;
-    if(i<fileList.size()){
-        ClientConnection::sendMsg(fd,fileList[i]);
-        std::cout<<ClientConnection::recvMsg(fd);
-    }else{
-        std::cout<<"there is no such file.\n";
-    }
-}
 
 int main(int argc, char* argv[])
 {
@@ -101,7 +12,7 @@ int main(int argc, char* argv[])
         std::cout<<"please input with format: client host\nexample: client 127.0.0.1\n";
         return 0;
     }
-    ClientConnection conn(TCP_TYPE,address);
+    TCPConnection conn(address.c_str());
     std::string msg;
     std::vector<std::string>fileList;
     std::string user_name,user_password;
@@ -115,9 +26,9 @@ int main(int argc, char* argv[])
 #endif
 
     msg="%"+user_name+"%"+user_password;
-    ClientConnection::sendMsg(conn.fd,msg);
+    Connection::sendMsg(conn.getFd(),msg);
     std::cout<<msg<<std::endl;
-    msg=ClientConnection::recvMsg(conn.fd);
+    msg=Connection::recvMsg(conn.getFd());
 
 #ifdef DEBUG_TIME
     std::cout<<msg<<std::endl;
@@ -128,7 +39,7 @@ int main(int argc, char* argv[])
         return 0;
     }
     while(1) {
-        msg=ClientConnection::recvMsg(conn.fd);
+        msg=Connection::recvMsg(conn.getFd());
         if(msg.compare(0,5,"file:")==0)
             fileList.push_back(msg.substr(5));
         else
@@ -142,16 +53,16 @@ int main(int argc, char* argv[])
             case CLIENT_EOF:
             break;
             case DOWLOAD_FILE:
-            ClientConnection::sendMsg(conn.fd,std::to_string(cmd));
-            recvFile(conn.fd, address, fileList);
+            Connection::sendMsg(conn.getFd(),std::to_string(cmd));
+            recvFile(conn.getFd(), address, fileList);
             break;
             case UPLOAD_FILE:
-            ClientConnection::sendMsg(conn.fd,std::to_string(cmd));
-            sendFile(conn.fd, address, fileList);
+            Connection::sendMsg(conn.getFd(),std::to_string(cmd));
+            sendFile(conn.getFd(), address, fileList);
             break;
             case DELETE_FILE:
-            ClientConnection::sendMsg(conn.fd,std::to_string(cmd));
-            delFile(fd,fileList);
+            Connection::sendMsg(conn.getFd(),std::to_string(cmd));
+            delFile(conn.getFd(),fileList);
             break;
             default:
             std::cout<<"unrecognized command.\n";
@@ -159,6 +70,6 @@ int main(int argc, char* argv[])
         }
     }while (cmd!=CLIENT_EOF);
     std::cout<<"exit.\n";
-    conn.close_self();
+    conn.closeSelf();
     return 0;
 }
