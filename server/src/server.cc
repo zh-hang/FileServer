df2c985f30b364481396a2abbcfdc5f6cf9f2e42
zhang-hang <zhang-hang@example.com>
2021-08-27 08:19:06 +0000

    粗略重构了了server端的代码

---
+++ server/src/server.cc
@@ -0,0 +1,34 @@
+#include "server.h"
+
+
+ServerOpt server_opt[] = {
+    {DOWLOAD_FILE,sendFile},
+    {UPLOAD_FILE,recvFile},
+    {DELETE_FILE,delFile},
+    {CLIENT_EOF, nullptr},
+};
+
+void sendFile(int fd,UDPConnection &file_connection){
+    std::string send_filename(Connection::recvMsg(fd));
+    Connection::sendMsg(fd,std::to_string(file_connection.getPort()));
+    sockaddr*ra=new sockaddr();
+    std::cout<<Connection::recvMsg(file_connection.getFd(),ra);
+    file_connection.sendFile(send_filename, ra);
+    delete ra;
+}
+
+void recvFile(int fd,UDPConnection &file_connection){
+    std::string recv_filename(Connection::recvMsg(fd));
+    Connection::sendMsg(fd,std::to_string(file_connection.getPort()));
+    sockaddr*ra=new sockaddr();
+    Connection::recvMsg(file_connection.getFd(),ra);
+    file_connection.recvFile(recv_filename);
+    delete ra;
+}
+
+void delFile(int fd,UDPConnection &file_connection){
+	std::string del_filename(Connection::recvMsg(fd));
+	int res=remove(del_filename.c_str());
+	Connection::sendMsg(fd,res?"success":"fail");	
+}
+
